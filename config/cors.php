6b176485c9e87ecb0babf1fcb8e048b017d19c2b
Bean <qaz326978547@gmail.com>
2024-08-30 18:09:44 +0800

    [add] CORS設定

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:5173'],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +29,8 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true, //將此設定為 true，以啟用對 CORS 要求的支持。
+
+
 
 ];
