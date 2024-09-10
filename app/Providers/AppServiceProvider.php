3875e997746179e6994c3e8ff24c934cc9c5521f
Bean <qaz326978547@gmail.com>
2024-09-10 17:42:35 +0800

    [delete] 刪除passportKey生成

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,29 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->createPassportKeys();
-    }
-    protected function createPassportKeys()
-    {
-        $privateKeyPath = storage_path('oauth-private.key');
-        $publicKeyPath = storage_path('oauth-public.key');
-
-        // Check if the private key file does not exist
-        if (!File::exists($privateKeyPath)) {
-            $privateKey = str_replace('\n', "\n", env('PASSPORT_PRIVATE_KEY'));
-            if ($privateKey) {
-                File::put($privateKeyPath, $privateKey);
-                chmod($privateKeyPath, 0600); // Set appropriate permissions
-            }
-        }
-
-        // Check if the public key file does not exist
-        if (!File::exists($publicKeyPath)) {
-            $publicKey = str_replace('\n', "\n", env('PASSPORT_PUBLIC_KEY'));
-            if ($publicKey) {
-                File::put($publicKeyPath, $publicKey);
-                chmod($publicKeyPath, 0600); // Set appropriate permissions
-            }
-        }
+        
     }
 }
